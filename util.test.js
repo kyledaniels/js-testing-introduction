abb4f0209f2b4ccf6917b9361658f4695753dd18
kyle67 <daniels.kyle@gmail.com>
2020-02-26 00:39:48 -0500

    Wrote unit test for util.js

--- util.test.js
+++ util.test.js
@@ -1,6 +1,13 @@
-const { generateText } = require('./util')
+// const { generateText } = require('./util')
+
+// test ('should output name and age', ()=>{
+//     const text = generateText ('Max',29);
+//     expect(text).toBe('Max (29 years old)');
+// });
+
+const {generateText} = require('./util.js')
 
 test ('should output name and age', ()=>{
-    const text = generateText ('Max',29);
-    expect(text).toBe('Max (29 years old)');
+     const text = generateText ('Max', 29);
+     expect(text).toBe('Max (29 years old)');
 });
